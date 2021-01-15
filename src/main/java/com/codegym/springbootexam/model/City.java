c0db7ed66685fafd9bddc1c2ee5bedc27d8c7852
anhnbt-it <anhnbt.it@gmail.com>
2021-01-15 09:01:54 +0700

    Add Entity

---
+++ src/main/java/com/codegym/springbootexam/model/City.java
@@ -0,0 +1,75 @@
+package com.codegym.springbootexam.model;
+
+import javax.persistence.*;
+
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "country_id", referencedColumnName = "id")
+    private Country country;
+    private String dienTich;
+    private String danSo;
+    private String gdp;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getDienTich() {
+        return dienTich;
+    }
+
+    public void setDienTich(String dienTich) {
+        this.dienTich = dienTich;
+    }
+
+    public String getDanSo() {
+        return danSo;
+    }
+
+    public void setDanSo(String danSo) {
+        this.danSo = danSo;
+    }
+
+    public String getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(String gdp) {
+        this.gdp = gdp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
